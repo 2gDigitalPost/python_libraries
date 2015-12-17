ef7431a58727f083f7f2a5e2ebf818deac13230d
topher.hughes <topher.hughes@2gdigital.com>
2015-12-17 13:20:00 -0800

    initial commit

---
+++ tacticenv/tactic_paths.py
@@ -0,0 +1,17 @@
+###########################################################
+#
+# Copyright (c) 2005, Southpaw Technology
+#                     All Rights Reserved
+#
+# PROPRIETARY INFORMATION.  This software is proprietary to
+# Southpaw Technology, and is not to be reproduced, transmitted,
+# or disclosed in any way without written permission.
+#
+#
+#
+
+
+TACTIC_INSTALL_DIR='/opt/spt/tactic'
+TACTIC_SITE_DIR='/opt/spt/projects'
+TACTIC_DATA_DIR='/opt/spt/tactic_data'
+
